8ad65b4f8649b74bf282409e9592db4eb63a11b5
MacBook Pro <macbookpro@sroo-punya.local>
2026-01-07 13:41:10 +0700

    fix github workflow

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL!;
+export const baseApiUrl =
+  process.env.NEXT_PUBLIC_BASE_API_URL! ?? "http://localhost:3000";
 export const apiUrl = `${baseApiUrl}/api/panel`;
-export const cookiesKey = process.env.NEXT_PUBLIC_COOKIES_KEY!;
+export const cookiesKey = process.env.NEXT_PUBLIC_COOKIES_KEY! ?? "secret";
