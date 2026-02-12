ae6565e8d61e596bb3a70882616355e332febb67
MacBook Pro <macbookpro@sroo-punya.local>
2026-02-13 02:35:00 +0700

    fix some error

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
-export const baseUrlToko = process.env.NEXT_PUBLIC_BASE_URL_TOKO!;
-export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
-export const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL!;
+export const baseUrlToko =
+  process.env.NEXT_PUBLIC_BASE_URL_TOKO! ?? "http://localhost:3000";
+export const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL! ?? "http://localhost:3001";
+export const baseApiUrl =
+  process.env.NEXT_PUBLIC_BASE_API_URL! ?? "https://api.bulky.id";
 export const apiUrl = `${baseApiUrl}/api/panel`;
 export const cookiesKey = process.env.NEXT_PUBLIC_COOKIES_KEY!;
